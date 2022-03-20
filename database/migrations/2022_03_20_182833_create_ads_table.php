636d12a6c1bc78c7894df51601c9924ebf2be866
MishaR <mesharoman12@gmail.com>
2022-03-20 20:51:46 +0200

    Migrations and models for categories and ads

---
+++ database/migrations/2022_03_20_182833_create_ads_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\User;
+use App\Models\Category;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('address');
+            $table->integer('price');
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->foreignIdFor(Category::class, 'category_id');
+            $table->timestamps();
+            $table->timestamp('expire_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+};
