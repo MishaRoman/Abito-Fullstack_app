e2422b79b66d0b154e7db8733f8da9810eea08bc
MishaR <mesharoman12@gmail.com>
2022-07-19 16:24:56 +0300

    404 page

--- vue/src/axios.js
+++ vue/src/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import store from './store'
+import router from "./router";
 
 const axiosClient = axios.create({
   baseURL: 'http://127.0.0.1:8000/api',
@@ -8,6 +9,15 @@ const axiosClient = axios.create({
 axiosClient.interceptors.request.use(config => {
   config.headers.Authorization = `Bearer ${store.state.user.token}`
   return config
+})
+
+axiosClient.interceptors.response.use(response => {
+  return response;
+}, error => {
+  if (error.response.status === 404) {
+    router.push({name: 'NotFound'})
+  }
+  return error;
 })
 
 export default axiosClient
